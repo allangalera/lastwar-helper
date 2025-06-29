471af299913c5da2d608971e809436cde6beb15c
Allan Galera <allangalera@gmail.com>
2025-06-29 20:00:17 +0200

    fix: Remove local postgres env var from code

--- src/environmentVariables.ts
+++ src/environmentVariables.ts
@@ -6,9 +6,6 @@ import { expand } from "dotenv-expand";
 const envVarSchema = z.object({
   BETTER_AUTH_URL: z.url(),
   DATABASE_URL: z.string(),
-  POSTGRES_PASSWORD: z.string(),
-  POSTGRES_USER: z.string(),
-  POSTGRES_DB: z.string(),
   GOOGLE_CLIENT_ID: z.string(),
   GOOGLE_CLIENT_SECRET: z.string(),
   DISCORD_CLIENT_ID: z.string(),
@@ -22,13 +19,13 @@ const EnvVarResult = envVarSchema.safeParse({
   ...process.env,
 });
 
-// if (EnvVarResult.error) {
-//   console.dir(z.prettifyError(EnvVarResult.error), {
-//     depth: null,
-//     colors: true,
-//   });
-//   process.exit(1);
-// }
+if (EnvVarResult.error) {
+  console.dir(z.prettifyError(EnvVarResult.error), {
+    depth: null,
+    colors: true,
+  });
+  process.exit(1);
+}
 
 console.log(EnvVarResult.data);
 
