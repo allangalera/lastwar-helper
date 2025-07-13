cb1ad24980ba8acc631745de662f6c5641d349ac
Allan Galera <allangalera@gmail.com>
2025-07-13 20:54:33 +0200

    feat: footer align center

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,8 +1,10 @@
 export const Footer = () => {
   return (
     <footer class="flex flex-col items-center gap-1 py-2">
-      <p>Made with ❤️ by © {new Date().getFullYear()} Aragantinis #1591</p>
-      <p>
+      <p class="text-center">
+        Made with ❤️ by © {new Date().getFullYear()} Aragantinis #1591
+      </p>
+      <p class="text-center">
         Not affiliated with{" "}
         <a href="https://www.lastwar.com/" rel="noreferrer">
           Last War: Survival
