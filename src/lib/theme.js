c9eb453bb6bbd3711c39d49a633fb43f8f14fb7a
Allan Galera <allangalera@gmail.com>
2025-07-13 22:56:01 +0200

    fix: theme color

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -5,8 +5,8 @@ document.documentElement.dataset.theme = theme;
 const meta = document.createElement('meta');
 meta.name = "theme-color";
 if (theme === "dark") {
-    meta.cotent = "#020617"
+    meta.content = "oklch(12.9% 0.042 264.695)"
 } else {
-    meta.cotent = "#e2e8f0"
+    meta.content = "oklch(92.9% 0.013 255.508)"
 }
 document.getElementsByTagName('head')[0].appendChild(meta);
