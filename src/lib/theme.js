a914d42457d98c83ca95d6aa8d4a2710a64b489b
Allan Galera <allangalera@gmail.com>
2025-07-13 22:26:49 +0200

    feat: Try to set meta theme color

--- src/lib/theme.js
+++ src/lib/theme.js
@@ -1,3 +1,12 @@
 const theme = localStorage.getItem("theme") || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
 
 document.documentElement.dataset.theme = theme;
+
+const meta = document.createElement('meta');
+meta.name = "theme-color";
+if (theme === "dark") {
+    meta.cotent = "#020617"
+} else {
+    meta.cotent = "#e2e8f0"
+}
+document.getElementsByTagName('head')[0].appendChild(meta);
