fbd4d2227f723312b20346d261a6a1747ab62a31
Allan Galera <allangalera@gmail.com>
2025-07-14 18:17:06 +0200

    feat: Modify footer to include Scarmoon

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,10 +1,11 @@
 export const Footer = () => {
   return (
     <footer class="flex flex-col items-center gap-1 py-2">
-      <p class="text-center">
-        Made with ❤️ by © {new Date().getFullYear()} Aragantinis #1591
+      <p class="text-balance text-center">
+        Made with ❤️ by © {new Date().getFullYear()} Aragantinis #1591 and
+        Scarmoon #1589
       </p>
-      <p class="text-center">
+      <p class="text-center text-balance">
         Not affiliated with{" "}
         <a href="https://www.lastwar.com/" rel="noreferrer">
           Last War: Survival
