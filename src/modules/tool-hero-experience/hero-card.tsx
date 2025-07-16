5f14bef156c66211e55d67ea5fe29e279df173db
Allan Galera <allangalera@gmail.com>
2025-07-16 19:43:59 +0200

    fix: Avoid breaking ui when missing info

--- src/modules/tool-hero-experience/hero-card.tsx
+++ src/modules/tool-hero-experience/hero-card.tsx
@@ -1,5 +1,6 @@
 import { pascalCase } from "change-case";
 import { HeroCategory, HeroInformation, HeroNames } from "./state";
+import { Show } from "solid-js";
 
 type HeroCardProps = {
   hero: HeroNames;
@@ -9,48 +10,49 @@ type HeroCardProps = {
 };
 
 export function HeroCard(props: HeroCardProps) {
+  const heroInformation = () => HeroInformation[props.hero];
   return (
-    <div
-      class="flex flex-col border rounded-xs w-30 gap-1"
-      classList={{
-        "bg-amber-600":
-          HeroInformation[props.hero].category === HeroCategory.UR,
-        "bg-purple-600":
-          HeroInformation[props.hero].category === HeroCategory.SSR,
-        "bg-sky-600": HeroInformation[props.hero].category === HeroCategory.SR,
-      }}
-    >
-      <div class="grid grid-cols-[25px_1fr_25px] items-center">
-        <div>
+    <Show when={heroInformation()}>
+      <div
+        class="flex flex-col border rounded-xs w-30 gap-1"
+        classList={{
+          "bg-amber-600": heroInformation().category === HeroCategory.UR,
+          "bg-purple-600": heroInformation().category === HeroCategory.SSR,
+          "bg-sky-600": heroInformation().category === HeroCategory.SR,
+        }}
+      >
+        <div class="grid grid-cols-[25px_1fr_25px] items-center">
+          <div>
+            <img
+              src={`/assets/${pascalCase(
+                heroInformation().capability
+              )}_Capability.webp`}
+            />
+          </div>
+          <div class="flex-1 text-center text-xs font-medium">
+            {pascalCase(props.hero)}
+          </div>
+          <div>
+            <img
+              src={`/assets/${pascalCase(heroInformation().type)}_Type.webp`}
+            />
+          </div>
+        </div>
+        <div class="flex justify-center">
           <img
-            src={`/assets/${pascalCase(
-              HeroInformation[props.hero].capability
-            )}_Capability.webp`}
+            src={`/assets/heroes/${pascalCase(props.hero)}_Thumbnail.webp`}
           />
         </div>
-        <div class="flex-1 text-center text-xs font-medium">
-          {pascalCase(props.hero)}
-        </div>
-        <div>
-          <img
-            src={`/assets/${pascalCase(
-              HeroInformation[props.hero].type
-            )}_Type.webp`}
+        <div class="flex p-2 gap-2">
+          <p>Lv.</p>
+          <input
+            class="border w-full text-center appearance-none"
+            type="number"
+            value={props.level}
+            onInput={(e) => props.setLevel(e.target.valueAsNumber)}
           />
         </div>
       </div>
-      <div class="flex justify-center">
-        <img src={`/assets/heroes/${pascalCase(props.hero)}_Thumbnail.webp`} />
-      </div>
-      <div class="flex p-2 gap-2">
-        <p>Lv.</p>
-        <input
-          class="border w-full text-center appearance-none"
-          type="number"
-          value={props.level}
-          onInput={(e) => props.setLevel(e.target.valueAsNumber)}
-        />
-      </div>
-    </div>
+    </Show>
   );
 }
