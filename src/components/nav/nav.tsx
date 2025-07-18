93948866655b77d3e7c40ee2ba876547b0dba83f
Allan Galera <allangalera@gmail.com>
2025-07-19 00:08:00 +0200

    feat: Make header site name link to home

--- src/components/nav/nav.tsx
+++ src/components/nav/nav.tsx
@@ -1,10 +1,10 @@
-import { ClientOnly } from "@tanstack/solid-router";
+import { ClientOnly, Link } from "@tanstack/solid-router";
 import { ThemeMenu } from "../theme-menu";
 
 export const Nav = () => {
   return (
     <header class="w-screen fixed flex justify-center top-0 p-2 gap-3 bg-slate-100 dark:bg-slate-900 z-100">
-      <div>Last War Helper</div>
+      <Link to="/">Last War Helper</Link>
       <ClientOnly>
         <ThemeMenu />
       </ClientOnly>
